5d792b5403dc6241864f4231ab571ae95259ab2b
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2024-07-04 22:07:13 -0500

    Use correct name and version in task

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,8 +48,8 @@ tasks.jar {
     manifest {
         attributes(
             mapOf(
-                "Implementation-Title" to name,
-                "Implementation-Version" to version
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version
             )
         )
     }
